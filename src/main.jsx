474904a6a08800fc17fa99ac0f4b09a02b061808
gitHowardhub <howardchavarro@gmail.com>
2026-08-02 21:28:52 -0500

    Add Bootstrap CSS import to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
@@ -16,4 +17,5 @@ createRoot(document.getElementById("root")).render(
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
+  
 );
